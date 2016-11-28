e6b735559d5b81296883e6413b60e8919f9cc9bb
jruchala <jruchala@gmail.com>
2016-11-28 16:45:17 -0500

    added info panel to household details view

--- Budgeter/Controllers/HomeController.cs
+++ Budgeter/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Budgeter.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,12 @@ namespace Budgeter.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
+            ViewBag.HouseholdCount = db.Housholds.Count();
+            ViewBag.UserCount = db.Users.Count();
             return View();
         }
 
