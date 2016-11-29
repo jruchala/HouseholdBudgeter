8f49b6e375e5476591327f11ebebe83fd42b8505
jruchala <jruchala@gmail.com>
2016-11-29 10:34:37 -0500

    create Invitation partial view

--- Budgeter/Models/Invitation.cs
+++ Budgeter/Models/Invitation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace Budgeter.Models
     public class Invitation
     {
         public int Id { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
         public string Code { get; set; }
 
