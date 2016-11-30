99b22ff76c98e248261a1e53c35da422635a34d3
jruchala <jruchala@gmail.com>
2016-11-30 14:52:11 -0500

    add accounts details view

--- Budgeter/Models/Transaction.cs
+++ Budgeter/Models/Transaction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,6 +12,8 @@ namespace Budgeter.Models
         public int Id { get; set; }
         public string Description { get; set; }
         public DateTime Date { get; set; }
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:C}")]
         public decimal Amount { get; set; }
         public string Type { get; set; }
         public bool Reconciled { get; set; }
